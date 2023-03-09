00051d7cd8540ba2a58cab36007067caaddee4bf
TienVoNguyen <nguyentienvo.97@gmail.com>
2023-03-09 17:12:22 +0700

    add to cart

--- OrangeShopFE/src/views/user/component/ProductCard/index.vue
+++ OrangeShopFE/src/views/user/component/ProductCard/index.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
-    <div class="card product-item border-0 mb-4">
-      <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+    <div class="card product-item border-0 mb-4 d-flex align-items-stretch">
+      <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0" style="max-height: 25em">
         <img class="img-fluid w-100" :src="imgProduct" alt="">
-        <h6 class="text-truncate mb-3 d-flex justify-content-center">{{ product.name }}</h6>
-        <div class="d-flex justify-content-center">
-          <h6>{{ product.price2 | currency('VND', 0, 'đ', '.', ',') }}</h6>
-          <h6 class="text-muted ml-2"><del>{{ product.price1 | currency('VND', 0, 'đ', '.', ',') }}</del></h6>
-        </div>
       </div>
-      <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+      <div class="card-body border-left border-right text-center p-0 pt-4 pb-3" style="min-height: 14em">
         <div class="d-flex justify-content-center">
+          <h6 class="text-truncate mb-3">{{ productCard.name }}</h6>
+        </div>
+        <div class="d-flex justify-content-center mb-3">
+          <h6>{{ productPriceSale | currency('VND', 0, 'đ', '.', ',') }}</h6><h6 class="text-muted ml-2"><del>{{ productPriceDefault | currency('VND', 0, 'đ', '.', ',') }}</del></h6>
+        </div>
+        <div class="d-flex justify-content-center mb-2">
           <el-radio-group v-model="selectedSize" size="mini" fill="#D19C97" @change="chooseSize">
-            <el-radio-button v-for="(s) in product.sizes" :key="s.id" :label="s.id">{{ s.name }}</el-radio-button>
+            <el-radio-button v-for="(s) in sizes.value" :key="s" :label="s" />
           </el-radio-group>
         </div>
-        <div class="d-flex justify-content-center w-100">
+        <div class="d-flex justify-content-center align-items-end w-100">
           <el-radio-group v-model="selectedColor" size="mini" fill="#D19C97" @change="chooseColor">
-            <el-radio-button v-for="(c) in product.colors" :key="c.id" :label="c.id">{{ c.name }}</el-radio-button>
+            <el-radio-button v-for="(c) in colors.value" :key="c" :label="c" />
           </el-radio-group>
         </div>
       </div>
@@ -25,7 +26,7 @@
         <el-button type="button" class="linkCard text-dark p-0 border-0 bg-light" @click="viewDetail(product)">
           <i class="fas fa-eye mr-1" style="color: #D19C97" />View Detail
         </el-button>
-        <el-button type="button" class="linkCard text-dark p-0 border-0 bg-light" @click="addToCart(product, selectedSize, selectedColor)">
+        <el-button type="button" class="linkCard text-dark p-0 border-0 bg-light" @click="addToCart(productAddToCard, selectedSize, selectedColor)">
           <i class="fas fa-shopping-cart mr-1" style="color: #D19C97" />Add To Cart
         </el-button>
       </div>
@@ -51,13 +52,18 @@ export default {
   },
   data() {
     return {
-      selectedSize: null,
-      selectedColor: null,
+      selectedSize: '',
+      selectedColor: '',
       sizes: [],
       colors: [],
-      variationOptions: [
-        { name: '', value: [] }
-      ]
+      variationOptions: [],
+      productAddToCard: {
+        id: '',
+        name: '',
+        priceSale: 0,
+        priceDefault: 0,
+        quantity: 1
+      }
     }
   },
   computed: {
@@ -66,64 +72,132 @@ export default {
       'token'
     ]),
     imgProduct() {
-      return require(`@/assets/img/product-1.jpg`)
+      return require(`@/assets/pictures/${this.productCard.defaultImage}`)
+    },
+    productPriceSale() {
+      return this.productAddToCard.priceSale === 0 ? this.productCard.productDetails[0].priceSale : this.productAddToCard.priceSale
+    },
+    productPriceDefault() {
+      return this.productAddToCard.priceDefault === 0 ? this.productCard.productDetails[0].priceDefault : this.productAddToCard.priceDefault
+    }
+  },
+  watch: {
+    selectedSize: function(val) {
+      if (this.selectedColor) {
+        // Duyệt qua các phần tử trong mảng "productDetails"
+        for (let i = 0; i < this.productCard.productDetails.length; i++) {
+          const detail = this.productCard.productDetails[i]
+          let hasSizeM = false
+          let hasColorBlack = false
+
+          // Kiểm tra xem phần tử có tuỳ chọn size 'M' hay không
+          for (let j = 0; j < detail.variationOptions.length; j++) {
+            if (detail.variationOptions[j].value === this.selectedSize && detail.variationOptions[j].variation.name === this.sizes.name) {
+              hasSizeM = true
+              break
+            }
+          }
+
+          // Kiểm tra xem phần tử có tuỳ chọn color 'Black' hay không
+          for (let j = 0; j < detail.variationOptions.length; j++) {
+            if (detail.variationOptions[j].value === this.selectedColor && detail.variationOptions[j].variation.name === this.colors.name) {
+              hasColorBlack = true
+              break
+            }
+          }
+
+          // Nếu phần tử có cả hai tuỳ chọn này, lấy thông tin chi tiết của phần tử đó
+          if (hasSizeM && hasColorBlack) {
+            this.productAddToCard.priceSale = detail.priceSale
+            this.productAddToCard.priceDefault = detail.priceDefault
+            this.productAddToCard.id = detail.id
+            break
+          }
+        }
+      }
+    },
+    selectedColor: function(val) {
+      if (this.selectedSize) {
+        // Duyệt qua các phần tử trong mảng "productDetails"
+        for (let i = 0; i < this.productCard.productDetails.length; i++) {
+          const detail = this.productCard.productDetails[i]
+          let hasSizeM = false
+          let hasColorBlack = false
+
+          // Kiểm tra xem phần tử có tuỳ chọn size 'M' hay không
+          for (let j = 0; j < detail.variationOptions.length; j++) {
+            if (detail.variationOptions[j].value === this.selectedSize && detail.variationOptions[j].variation.name === this.sizes.name) {
+              hasSizeM = true
+              break
+            }
+          }
+
+          // Kiểm tra xem phần tử có tuỳ chọn color 'Black' hay không
+          for (let j = 0; j < detail.variationOptions.length; j++) {
+            if (detail.variationOptions[j].value === this.selectedColor && detail.variationOptions[j].variation.name === this.colors.name) {
+              hasColorBlack = true
+              break
+            }
+          }
+
+          // Nếu phần tử có cả hai tuỳ chọn này, lấy thông tin chi tiết của phần tử đó
+          if (hasSizeM && hasColorBlack) {
+            this.productAddToCard.priceSale = detail.priceSale
+            this.productAddToCard.priceDefault = detail.priceDefault
+            this.productAddToCard.id = detail.id
+            break
+          }
+        }
+      }
     }
   },
   created() {
-    // this.productCard.productDetails.forEach(options => {
-    //   console.log(options)
-    //   options.variationOptions.forEach(option => {
-    //     if (option.variation.name === 'Color') {
-    //       this.colors.push(option.value)
-    //     } else if (option.variation.name === 'Size') {
-    //       this.sizes.push(option.value)
-    //     }
-    //   })
-    // })
-    // let options = {};
-    //
-    // for (let i = 0; i < productDetails.length; i++) {
-    //   let variationOptions = productDetails[i].variationOptions;
-    //
-    //   for (let j = 0; j < variationOptions.length; j++) {
-    //     let variation = variationOptions[j].variation;
-    //     let value = variationOptions[j].value;
-    //
-    //     if (!options[variation.name]) {
-    //       options[variation.name] = [];
-    //     }
-    //
-    //     options[variation.name].push(value);
-    //   }
-    // }
-    //
-    // console.log(options);
-    const variations = this.productCard.productDetails.map(detail => {
-      return detail.variationOptions.reduce((result, option) => {
-        const variation = option.variation.name
-        const value = option.value
-
-        if (!result.hasOwnProperty(variation)) {
-          result[variation] = []
-        }
-
-        result[variation].push(value)
-
-        return result
-      }, {})
-    })
-
-    console.log(variations)
+    console.log(this.productCard)
+    this.getProductVariations()
+    this.productAddToCard.name = this.productCard.name
+    this.productAddToCard.priceSale = this.productCard.productDetails[0].priceSale
+    this.productAddToCard.priceDefault = this.productCard.productDetails[0].priceDefault
+    console.log(1, this.productAddToCard)
+    console.log(2, this.productCard.productDetails[0])
   },
   methods: {
     chooseSize() {
-      console.log(this.selectedSize)
+      // console.log(this.selectedSize)
     },
     chooseColor() {
-      console.log(this.selectedColor)
+      // console.log(this.selectedColor)
     },
     viewDetail(data) {
       this.$router.push(`/product`)
+    },
+    getProductVariations() {
+      const options = {}
+      let productVariations = []
+      for (let i = 0; i < this.productCard.productDetails.length; i++) {
+        const variationOptions = this.productCard.productDetails[i].variationOptions
+
+        for (let j = 0; j < variationOptions.length; j++) {
+          const variation = variationOptions[j].variation
+          const value = variationOptions[j].value
+          if (!options[variation.name]) {
+            options[variation.name] = []
+          }
+          options[variation.name].push(value)
+        }
+      }
+      productVariations = options
+
+      for (const property in productVariations) {
+        const obj = {}
+        obj.name = property
+        obj.value = productVariations[property].filter((item, index) => {
+          return productVariations[property].indexOf(item) === index
+        })
+        this.variationOptions.push(obj)
+      }
+      this.sizes = this.variationOptions[0]
+      this.colors = this.variationOptions[1]
+      // console.log(this.variationOptions)
     }
   }
 }
@@ -145,6 +219,13 @@ export default {
   color: #D19C97 !important;
   text-decoration: none;
   background: none !important;
+}
+
+.text-truncate {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: normal;
+  text-align: center;
 }
 
 </style>
